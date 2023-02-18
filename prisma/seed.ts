d1a5b39f233f33c26f09c7e20bbd967f9ff5081f
Rasmus Pedersen <18396856+rpede@users.noreply.github.com>
2023-02-19 00:23:18 +0100

    Add another company to seed and changes passwords

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,65 +1,87 @@
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
+function saveUser(
+  data: Prisma.UserCreateInput | Prisma.UserUncheckedCreateInput
+) {
+  return prisma.user.upsert({
+    where: { email: data.email },
+    update: data,
+    create: data,
+  });
+}
+
+function saveCompany(
+  data: Prisma.CompanyCreateInput | Prisma.CompanyUncheckedCreateInput
+) {
+  return prisma.company.upsert({
+    where: { name: data.name },
+    update: data,
+    create: data,
+  });
+}
+
 async function main() {
-  const admin = {
+  await saveUser({
     email: 'admin@example.com',
-    name: 'God',
+    name: 'Admin',
     passwordHash:
-      '$2y$19$GHloi75EHUiV/xixFVG3jeDtlE/FriMZxeL/9cFfDxEYV50l2r1n6', // secret
+    '$2b$10$lJp7XSw2BiUJit/CBjPXieJXPQJWj01IY.966XnKDH3EDkdVno/o2', // god
     role: 'ADMIN',
-  };
-  await prisma.user.upsert({
-    where: { email: admin.email },
-    update: admin,
-    create: admin,
   });
 
-  const companyAData = {
-    name: 'Company A',
-  };
-  const companyA = await prisma.company.upsert({
-    where: { name: companyAData.name },
-    update: companyAData,
-    create: companyAData,
-  });
-
-  const michaelData = {
+  const companyA = await saveCompany({ name: 'Company A' });
+  await saveUser({
     email: 'michaelscott@a.com',
     name: 'Michael Scott',
     passwordHash: 'bdc87b9c894da5168059e00ebffb9077', // password1234
     role: 'COMPANY',
     companyId: companyA.id,
-  };
-  await prisma.user.upsert({
-    where: { email: michaelData.email },
-    update: michaelData,
-    create: michaelData,
   });
-  const jimData = {
+  await saveUser({
     email: 'jim@a.com',
     name: 'Jim',
-    passwordHash: 'bdc87b9c894da5168059e00ebffb9077', // password1234
+    passwordHash: '54aea06e65a58b1bc96e060c86b212c3', // ilovepam
     role: 'NORMAL',
     companyId: companyA.id,
-  };
-  await prisma.user.upsert({
-    where: { email: jimData.email },
-    update: jimData,
-    create: jimData,
   });
-  const pamData = {
+  await saveUser({
     email: 'pam@a.com',
     name: 'Pam',
-    passwordHash: 'bdc87b9c894da5168059e00ebffb9077', // password1234
+    passwordHash: '8df2b7edb88ba2270a320df6651b1422', // ilovejim
     role: 'NORMAL',
     companyId: companyA.id,
-  };
-  await prisma.user.upsert({
-    where: { email: pamData.email },
-    update: pamData,
-    create: pamData,
+  });
+
+  const companyB = await saveCompany({ name: 'Company B' });
+  await saveUser({
+    email: 'boss@b.com',
+    name: 'Pointy-haired Boss',
+    passwordHash: 'f8f4877c5d8062c7ce207170d070f11b', // p3cafx4q
+    role: 'COMPANY',
+    companyId: companyB.id,
+  });
+  await saveUser({
+    email: 'dilbert@b.com',
+    name: 'Dilbert',
+    passwordHash: '4550955866ee7b328f9e0877bc37a096', // Ab4gSgWX
+    role: 'NORMAL',
+    companyId: companyB.id,
+  });
+  await saveUser({
+    email: 'wally@b.com',
+    name: 'Wally',
+    passwordHash: '3ee9f4b95b4c9e5276d4dba3df61e353', // B2WDEcBg
+    role: 'NORMAL',
+    companyId: companyB.id,
+  });
+  await saveUser({
+    email: 'alice@b.com',
+    name: 'Alice',
+    passwordHash: 'e2d0b6fa939e5ceec4217df543606144', // OsRxnTx]S8?!\K[O;/`zD<QB4lyk\vy0
+    role: 'NORMAL',
+    companyId: companyB.id,
   });
 }
 
